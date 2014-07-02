456c21de8a0d715666abfe1aaea7e79de22e5dc6
Caragus <caragus@Evgenys-MacBook-Air.local>
2014-07-03 00:31:27 +0400

    Update link to article

--- nginx.org/data/configuration.js
+++ nginx.org/data/configuration.js
@@ -3,6 +3,6 @@
 (function() {
     var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
     var _creator = function() { return _(b,c,d,c,e,f,g,c);}; 
-var b="showPageNotes",c=false,d="showPageNoteNames",e="logoImageCaption",f="Прототип <a href='http://nginx.org/'>nginx.org</a> для статьи «<a href='#'>Эффективный или эффектный? Мастер-класс по созданию дизайна сайта. Часть I</a>»",g="loadFeedbackPlugin";
+var b="showPageNotes",c=false,d="showPageNoteNames",e="logoImageCaption",f="Прототип <a href='http://nginx.org/'>nginx.org</a> для статьи «<a href='http://habrahabr.ru/post/228375/'>Эффективный или эффектный? Мастер-класс по созданию дизайна сайта. Часть I</a>»",g="loadFeedbackPlugin";
 return _creator();
 })()
